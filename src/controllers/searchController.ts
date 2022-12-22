eae0c0bdcf054d751ead95fae2a9ea775dacf515
Antonio Rafael <andrade.rafa71@gmail.com>
2022-12-22 11:46:10 -0300

    Final Project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,20 @@
 import {Request, Response} from 'express';
 
-export const search = (req: Request, res: Response) =>{
+import { Pet } from '../models/Pets';
+import { createMenuObject } from '../helpers/createMenuObject';
 
+export const search = (req: Request, res: Response) =>{
+    let query = req.query.q as string;
+    let list = Pet.getFromName(query)
+
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 }
